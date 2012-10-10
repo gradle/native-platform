6b09ac415a63f35dd1a55ed079cb1d267e9c3553
Adam Murdoch <adam.murdoch@gradleware.com>
2012-10-10 17:09:12 +1100

    Updated native library version.

--- src/main/java/net/rubygrapefruit/platform/internal/jni/NativeLibraryFunctions.java
+++ src/main/java/net/rubygrapefruit/platform/internal/jni/NativeLibraryFunctions.java
@@ -4,7 +4,7 @@ import net.rubygrapefruit.platform.internal.FunctionResult;
 import net.rubygrapefruit.platform.internal.MutableSystemInfo;
 
 public class NativeLibraryFunctions {
-    public static final int VERSION = 9;
+    public static final int VERSION = 10;
 
     public static native int getVersion();
 
