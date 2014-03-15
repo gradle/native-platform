8fabe5b0c436587b5907b2fd8eb3ec45cfc9ac8b
Adam Murdoch <adam.murdoch@gradleware.com>
2014-03-15 13:35:38 +1100

    Added a stubbed implementation to list filesystems on FreeBSD.

---
+++ src/main/cpp/freebsd.cpp
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2014 Adam Murdoch
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+/*
+ * FreeBSD specific functions.
+ */
+#ifdef __FreeBSD__
+
+#include "native.h"
+#include "generic.h"
+
+/*
+ * File system functions
+ */
+JNIEXPORT void JNICALL
+Java_net_rubygrapefruit_platform_internal_jni_PosixFileSystemFunctions_listFileSystems(JNIEnv *env, jclass target, jobject info, jobject result) {
+    mark_failed_with_message(env, "not implemented", result);
+}
+
+#endif
