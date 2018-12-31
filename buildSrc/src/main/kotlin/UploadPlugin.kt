cc42f3f252566f86640a34f8913cd5c27312cff2
Adam Murdoch <adam@gradle.com>
2018-12-31 11:48:51 +1100

    Separate out the upload tasks so that the JNI jars and the main jar can be uploaded separately.

--- buildSrc/src/main/kotlin/UploadPlugin.kt
+++ buildSrc/src/main/kotlin/UploadPlugin.kt
@@ -7,13 +7,15 @@ import org.gradle.api.publish.maven.MavenPublication
 import org.gradle.api.tasks.TaskAction
 import java.io.ByteArrayOutputStream
 import java.io.File
+import java.io.InputStream
 import java.net.HttpURLConnection
 import java.net.URL
 import java.util.*
 import java.util.concurrent.Callable
 
 /**
- * Uploads each publication of the project to bintray.
+ * Uploads each publication of the project to bintray. Uses the bintray API directly rather than the bintray plugin, as the plugin does not
+ * handle multiple packages per project.
  */
 class UploadPlugin : Plugin<Project> {
     override fun apply(project: Project) {
@@ -28,43 +30,47 @@ class UploadPlugin : Plugin<Project> {
             extension.repositories.maven { repo ->
                 repo.setUrl(repoDir.call())
             }
-        }
-        val upload = project.tasks.create("upload", UploadTask::class.java) { task ->
-            task.repoDir = repoDir
-            task.publications = object : Callable<Collection<MavenPublication>> {
-                override fun call(): Collection<MavenPublication> {
-                    val extension = project.extensions.getByType(PublishingExtension::class.java)
-                    return extension.publications.withType(MavenPublication::class.java)
+            extension.publications.withType(MavenPublication::class.java) { publication ->
+                val upload = project.tasks.create("upload${publication.name.capitalize()}", UploadTask::class.java) { task ->
+                    task.group = "Upload"
+                    task.description = "Upload publication ${publication.name}"
+                    task.repoDir = repoDir
+                    task.publication = publication
+                }
+                if (publication.name != "main") {
+                    val lifecycleTask = project.tasks.maybeCreate("uploadJni")
+                    lifecycleTask.group = "Upload"
+                    lifecycleTask.description = "Upload all JNI publications"
+                    lifecycleTask.dependsOn(upload)
                 }
             }
         }
         project.gradle.taskGraph.whenReady { graph ->
-            if (graph.hasTask(upload)) {
-                if (!project.hasProperty("bintrayUserName")) {
-                    throw IllegalStateException("Project property 'bintrayUserName' not provided.")
+            graph.allTasks.forEach { task ->
+                if (task is UploadTask) {
+                    if (!project.hasProperty("bintrayUserName")) {
+                        throw IllegalStateException("Project property 'bintrayUserName' not provided.")
+                    }
+                    task.userName = project.property("bintrayUserName").toString()
+                    if (!project.hasProperty("bintrayApiKey")) {
+                        throw IllegalStateException("Project property 'bintrayApiKey' not provided.")
+                    }
+                    task.apiKey = project.property("bintrayApiKey").toString()
                 }
-                upload.userName = project.property("bintrayUserName").toString()
-                if (!project.hasProperty("bintrayApiKey")) {
-                    throw IllegalStateException("Project property 'bintrayApiKey' not provided.")
-                }
-                upload.apiKey = project.property("bintrayApiKey").toString()
             }
         }
     }
 
     open class UploadTask : DefaultTask() {
-        lateinit var publications: Callable<Collection<MavenPublication>>
+        lateinit var publication: MavenPublication
         lateinit var repoDir: Callable<File>
         lateinit var userName: String
         lateinit var apiKey: String
 
         init {
-            val dependencies = object : Callable<Collection<Task>> {
-                override fun call(): Collection<Task> {
-                    val tasks: List<Task> = publications.call().map { publication ->
-                        project.tasks.getByName("publish${publication.name.capitalize()}PublicationToMavenRepository")
-                    }
-                    return tasks
+            val dependencies = object : Callable<Task> {
+                override fun call(): Task {
+                    return project.tasks.getByName("publish${publication.name.capitalize()}PublicationToMavenRepository")
                 }
             }
             dependsOn(dependencies)
@@ -72,13 +78,11 @@ class UploadPlugin : Plugin<Project> {
 
         @TaskAction
         fun upload() {
-            publications.call().forEach { publication ->
-                println("Uploading publication ${publication.name}")
-                publication.artifacts.forEach { artifact ->
-                    upload(publication.groupId, publication.artifactId, publication.version, artifact.classifier, artifact.extension)
-                }
-                upload(publication.groupId, publication.artifactId, publication.version, null, "pom")
+            println("Uploading publication ${publication.name}")
+            publication.artifacts.forEach { artifact ->
+                                upload(publication.groupId, publication.artifactId, publication.version, artifact.classifier, artifact.extension)
             }
+            upload(publication.groupId, publication.artifactId, publication.version, null, "pom")
         }
 
         private fun upload(groupId: String, artifactId: String, version: String, classifier: String?, extension: String) {
@@ -104,18 +108,23 @@ class UploadPlugin : Plugin<Project> {
             if (!file.isFile) {
                 throw IllegalArgumentException("Artifact ${baseName} file ${file} does not exist.")
             }
-            val uploadUrl = URL("https://api.bintray.com/maven/adammurdoch/maven/${groupId}:${artifactId}/${mavenPath}")
-            val connection = uploadUrl.openConnection() as HttpURLConnection
             file.inputStream().use { instr ->
-                withCredentials(connection)
-                connection.addRequestProperty("Content-Length", file.length().toString())
-                connection.requestMethod = "PUT"
-                connection.doOutput = true
-                instr.copyTo(connection.outputStream)
-                val collect = ByteArrayOutputStream()
-                connection.inputStream.copyTo(collect)
-                println(collect.toString())
+                val uploadUrl = uploadUrl(groupId, artifactId, mavenPath)
+                upload(uploadUrl, file.length(), instr)
             }
         }
+
+        private fun upload(uploadUrl: URL, length: Long, instr: InputStream): Long {
+            val connection = uploadUrl.openConnection() as HttpURLConnection
+            withCredentials(connection)
+            connection.addRequestProperty("Content-Length", length.toString())
+            connection.requestMethod = "PUT"
+            connection.doOutput = true
+            instr.copyTo(connection.outputStream)
+            val collect = ByteArrayOutputStream()
+            return connection.inputStream.copyTo(collect)
+        }
+
+        private fun uploadUrl(groupId: String, artifactId: String, mavenPath: String) = URL("https://api.bintray.com/maven/adammurdoch/maven/${groupId}:${artifactId}/${mavenPath}")
     }
 }
