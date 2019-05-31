d9588baf02bcd6fe2d0a5c7ee392fd38e56c47ee
Renaud Paquay <rpaquay@google.com>
2019-05-31 12:58:03 -0700

    Add tests for symbolic link support on Windows
    * The tests only run on Windows Vista+ in an elevated command prompt,
      or on Windows 10+ in developer mode.
    * See https://blogs.windows.com/buildingapps/2016/12/02/symlinks-windows-10
      for details on these restrictions.

--- src/test/groovy/net/rubygrapefruit/platform/file/AbstractFilesTest.groovy
+++ src/test/groovy/net/rubygrapefruit/platform/file/AbstractFilesTest.groovy
@@ -20,6 +20,7 @@ import net.rubygrapefruit.platform.internal.Platform
 import spock.lang.Specification
 
 import java.nio.file.LinkOption
+import java.nio.file.Paths
 import java.nio.file.attribute.BasicFileAttributeView
 import java.nio.file.attribute.BasicFileAttributes
 
@@ -37,5 +38,71 @@ class AbstractFilesTest extends Specification {
 
     void assertTimestampMatches(long statTime, long javaTime) {
         assert maybeRoundToNearestSecond(statTime) == maybeRoundToNearestSecond(javaTime)
+    }
+
+    static boolean supportsSymbolicLinks() {
+        if (!Platform.current().windows) {
+            return true
+        }
+
+        def createdFolder = File.createTempFile("junit", "")
+        createdFolder.delete()
+        createdFolder.mkdir()
+
+        def dir = createdFolder
+        def targetFile = new File(dir, "foo-dir")
+        def linkFile = new File(dir, "foo-dir.link")
+        targetFile.mkdirs()
+
+        try {
+            try {
+                createDirectorySymbolicLink(linkFile, targetFile.name)
+                return true
+            } catch(ignored) {
+                return false
+            }
+        }
+        finally {
+            targetFile.delete()
+            linkFile.delete()
+            dir.deleteDir()
+        }
+    }
+
+    private static void createSymbolicLinkHelper(File file, String linkTarget, boolean isDirectory) {
+        if (Platform.current().windows) {
+            // On Windows, we use the 'mklink' command, which works with both elevated processes
+            // and Windows 10 with developer mode enabled (see definition of the
+            // SYMBOLIC_LINK_FLAG_ALLOW_UNPRIVILEGED_CREATE flag at
+            // https://docs.microsoft.com/en-us/windows/desktop/api/winbase/nf-winbase-createsymboliclinkw).
+            //
+            // See https://blogs.windows.com/buildingapps/2016/12/02/symlinks-windows-10 for detailed
+            // information.
+            //
+            // Until the java.nio.file.Files.createSymbolicLink API support this flag, we need to resort
+            // to using the 'mklink' command. We are limited to path shorted than 260 characters though.
+            if (file.absolutePath.length() >= 260) {
+                throw new IllegalArgumentException("Error creating symbolic link: Path must be shorter than 260 characters on Windows")
+            }
+            def outputStringBuilder = new StringBuilder(), errStringBuilder = new StringBuilder()
+            def arg = isDirectory ? "/d " : ""
+            def cmd = "cmd /c mklink ${arg} \"${file.absolutePath}\" \"${linkTarget}\""
+            def process = cmd.execute()
+            process.consumeProcessOutput(outputStringBuilder, errStringBuilder)
+            process.waitForOrKill(2000)
+            if (process.exitValue() != 0) {
+                throw new IOException("Error creating symbolic link (${errStringBuilder.toString()})")
+            }
+        } else {
+            java.nio.file.Files.createSymbolicLink(file.toPath(), Paths.get(linkTarget))
+        }
+    }
+
+    static void createFileSymbolicLink(File file, String linkTarget) {
+        createSymbolicLinkHelper(file, linkTarget, false)
+    }
+
+    static void createDirectorySymbolicLink(File file, String linkTarget) {
+        createSymbolicLinkHelper(file, linkTarget, true)
     }
 }
