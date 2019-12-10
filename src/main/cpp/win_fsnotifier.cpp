cac071422abffa1f585e90e8c0d73661db27a9ca
Lóránt Pintér <lorant.pinter@gmail.com>
2019-12-10 15:25:31 +0100

    Use ReadDirectoryChangesW to listen to a single path

--- src/main/cpp/win_fsnotifier.cpp
+++ src/main/cpp/win_fsnotifier.cpp
@@ -13,6 +13,17 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+JavaVM* jvm = NULL;
+
+typedef struct watch_details {
+    HANDLE threadHandle;
+    HANDLE stopEventHandle;
+    char drivePath[4];
+    wchar_t *watchedPath;
+    jobject watcherCallback;
+} watch_details_t;
+
 typedef struct {
     char rootPath[4];
     HANDLE hThread;
@@ -395,76 +406,112 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-// File watching
+void handlePathChanged(watch_details_t *details, FILE_NOTIFY_INFORMATION *info) {
+   const char *event;
+    if (info->Action == FILE_ACTION_ADDED || info->Action == FILE_ACTION_RENAMED_OLD_NAME) {
+        event = "CREATE";
+    } else if (info->Action == FILE_ACTION_REMOVED || info->Action == FILE_ACTION_RENAMED_OLD_NAME) {
+        event = "DELETE";
+    } else if (info->Action == FILE_ACTION_MODIFIED) {
+        event = "CHANGE";
+    } else {
+        return;  // unknown event
+    }
 
-JavaVM* jvm = NULL;
+    char utfBuffer[4 * MAX_PATH + 1];
+    int wcsLen = info->FileNameLength / sizeof(wchar_t);
+    int converted = WideCharToMultiByte(CP_UTF8, 0, info->FileName, wcsLen, utfBuffer, sizeof(utfBuffer), NULL, NULL);
+    utfBuffer[converted] = '\0';
 
-typedef struct watch_details {
-    HANDLE watchHandle;
-    HANDLE threadHandle;
-    jstring path;
-    jobject watcherCallback;
-} watch_details_t;
+    printf("> Changed: %s\%s\n", details->drivePath, utfBuffer);
+
+    // JNIEnv* env;
+    // int getEnvStat = jvm->GetEnv((void **)&env, JNI_VERSION_1_6);
+    // if (getEnvStat == JNI_EDETACHED) {
+    //     int attachThreadStat = jvm->AttachCurrentThread((void **) &env, NULL);
+    //     if (attachThreadStat != JNI_OK) {
+    //         printf("~~~~ Problem with AttachCurrentThread: %d\n", attachThreadStat);
+    //         // TODO Error handling
+    //         // invalidStateDetected = true;
+    //         break;
+    //     }
+    // } else if (getEnvStat == JNI_EVERSION) {
+    //     printf("~~~~ Problem with GetEnv: %d\n", getEnvStat);
+    //     // TODO Error handling
+    //     // invalidStateDetected = true;
+    //     break;
+    // }
+    //
+    // wchar_t* pathStr = java_to_wchar_path(env, details->path, NULL);
+    // printf("~~~~ Changes: %ls\n", pathStr);
+    // free(pathStr);
+
+    // jclass callback_class = env->GetObjectClass(details->watcherCallback);
+    // jmethodID methodCallback = env->GetMethodID(callback_class, "pathChanged", "(Ljava/lang/String;)V");
+    // env->CallVoidMethod(details->watcherCallback, methodCallback, details->path);    
+}
 
 DWORD WINAPI EventProcessingThread(LPVOID data) {
     watch_details_t *details = (watch_details_t*) data;
-    while (TRUE) {
-        if (WaitForSingleObject(details->watchHandle, INFINITE) == WAIT_FAILED) {
+
+    printf("~~~~ Starting thread\n");
+
+    OVERLAPPED overlapped;
+    memset(&overlapped, 0, sizeof(overlapped));
+    overlapped.hEvent = CreateEvent(NULL, FALSE, FALSE, NULL);
+
+    const char *drivePath = details->drivePath;
+    HANDLE hDrive = CreateFileA(drivePath, GENERIC_READ, CREATE_SHARE, NULL, OPEN_EXISTING, CREATE_FLAGS, NULL);
+
+    char buffer[EVENT_BUFFER_SIZE];
+    HANDLE handles[2] = {details->stopEventHandle, overlapped.hEvent};
+    while (true) {
+        int rcDrive = ReadDirectoryChangesW(hDrive, buffer, sizeof(buffer), TRUE, EVENT_MASK, NULL, &overlapped, NULL);
+        if (rcDrive == 0) {
             // TODO Error handling
-            // mark_failed_with_errno(env, "could not wait for change notification", result);
-            break;
-        }
-        if (!FindNextChangeNotification(details->watchHandle)) {
-            if (GetLastError() == ERROR_INVALID_HANDLE) {
-                // Assumed closed
-                break;
-            }
-            // TODO Error handling
-            // mark_failed_with_errno(env, "could not schedule next change notification", result);
+            printf("~~~~ Couldn't read directory: %d\n", rcDrive);
             break;
         }
 
-        JNIEnv* env;
-        int getEnvStat = jvm->GetEnv((void **)&env, JNI_VERSION_1_6);
-        if (getEnvStat == JNI_EDETACHED) {
-            int attachThreadStat = jvm->AttachCurrentThread((void **) &env, NULL);
-            if (attachThreadStat != JNI_OK) {
-                printf("~~~~ Problem with AttachCurrentThread: %d\n", attachThreadStat);
+        DWORD rc = WaitForMultipleObjects(2, handles, FALSE, INFINITE);
+        if (rc == WAIT_OBJECT_0) {
+            break;
+        }
+        if (rc == WAIT_OBJECT_0 + 1) {
+            DWORD dwBytesReturned;
+            if (!GetOverlappedResult(hDrive, &overlapped, &dwBytesReturned, FALSE)) {
                 // TODO Error handling
-                // invalidStateDetected = true;
+                printf("~~~~ Failed to wait\n");
                 break;
             }
-        } else if (getEnvStat == JNI_EVERSION) {
-            printf("~~~~ Problem with GetEnv: %d\n", getEnvStat);
-            // TODO Error handling
-            // invalidStateDetected = true;
-            break;
+
+            if (dwBytesReturned == 0) {
+                // don't send dirty too much, everything is changed anyway
+                // TODO Understand what this does
+                if (WaitForSingleObject(details->stopEventHandle, 500) == WAIT_OBJECT_0)
+                    break;
+
+                // Got a buffer overflow => current changes lost => send RECDIRTY on root
+                // TODO Signal overflow
+            } else {
+                FILE_NOTIFY_INFORMATION *info = (FILE_NOTIFY_INFORMATION *)buffer;
+                do {
+                    handlePathChanged(details, info);
+                    info = (FILE_NOTIFY_INFORMATION *)((char *)info + info->NextEntryOffset);
+                } while (info->NextEntryOffset != 0);
+            }
         }
-
-        wchar_t* pathStr = java_to_wchar_path(env, details->path, NULL);
-        printf("~~~~ Changes: %ls\n", pathStr);
-        free(pathStr);
-
-        jclass callback_class = env->GetObjectClass(details->watcherCallback);
-        jmethodID methodCallback = env->GetMethodID(callback_class, "pathChanged", "(Ljava/lang/String;)V");
-        env->CallVoidMethod(details->watcherCallback, methodCallback, details->path);
     }
+
+    printf("~~~~ Stopping thread\n");
+
+    CloseHandle(overlapped.hEvent);
+    CloseHandle(hDrive);
     return 0;
 }
 
 JNIEXPORT jobject JNICALL
 Java_net_rubygrapefruit_platform_internal_jni_WindowsFileEventFunctions_startWatch(JNIEnv *env, jclass target, jstring path, jobject javaCallback, jobject result) {
-    wchar_t* pathStr = java_to_wchar_path(env, path, result);
-    HANDLE watchHandle = FindFirstChangeNotificationW(
-        pathStr,
-        TRUE,
-        FILE_NOTIFY_CHANGE_FILE_NAME | FILE_NOTIFY_CHANGE_DIR_NAME | FILE_NOTIFY_CHANGE_ATTRIBUTES | FILE_NOTIFY_CHANGE_SIZE | FILE_NOTIFY_CHANGE_LAST_WRITE
-    );
-    free(pathStr);
-    if (watchHandle == INVALID_HANDLE_VALUE) {
-        mark_failed_with_errno(env, "could not open change notification", result);
-        return NULL;
-    }
 
     // TODO Should this be somewhere global?
     int jvmStatus = env->GetJavaVM(&jvm);
@@ -473,11 +520,14 @@ Java_net_rubygrapefruit_platform_internal_jni_WindowsFileEventFunctions_startWat
         return NULL;
     }
 
-    watch_details_t* details = (watch_details_t*)malloc(sizeof(watch_details_t));
-    details->watchHandle = watchHandle;
-    details->path = (jstring) env->NewGlobalRef(path);
-    details->watcherCallback = env->NewGlobalRef(javaCallback);
+    wchar_t* pathStr = java_to_wchar_path(env, path, result);
+    char drivePath[4] = {toupper(pathStr[0]), ':', '\\', '\0'};
 
+    watch_details_t* details = (watch_details_t*)malloc(sizeof(watch_details_t));
+    details->watcherCallback = env->NewGlobalRef(javaCallback);
+    details->stopEventHandle = CreateEvent(NULL, FALSE, FALSE, NULL);
+    details->watchedPath = pathStr;
+    strcpy_s(details->drivePath, 4, drivePath);
     details->threadHandle = CreateThread(
         NULL,                   // default security attributes
         0,                      // use default stack size
@@ -486,6 +536,7 @@ Java_net_rubygrapefruit_platform_internal_jni_WindowsFileEventFunctions_startWat
         0,                      // use default creation flags
         NULL                    // the thread identifier
     );
+    SetThreadPriority(details->threadHandle, THREAD_PRIORITY_ABOVE_NORMAL);
 
     jclass clsWatch = env->FindClass("net/rubygrapefruit/platform/internal/jni/WindowsFileEventFunctions$WatchImpl");
     jmethodID constructor = env->GetMethodID(clsWatch, "<init>", "(Ljava/lang/Object;)V");
@@ -495,10 +546,12 @@ Java_net_rubygrapefruit_platform_internal_jni_WindowsFileEventFunctions_startWat
 JNIEXPORT void JNICALL
 Java_net_rubygrapefruit_platform_internal_jni_WindowsFileEventFunctions_stopWatch(JNIEnv *env, jclass target, jobject detailsObj, jobject result) {
     watch_details_t* details = (watch_details_t*)env->GetDirectBufferAddress(detailsObj);
-    env->DeleteGlobalRef(details->path);
-    env->DeleteGlobalRef(details->watcherCallback);
-    FindCloseChangeNotification(details->watchHandle);
+    SetEvent(details->stopEventHandle);
+    WaitForSingleObject(details->threadHandle, INFINITE);
     CloseHandle(details->threadHandle);
+    CloseHandle(details->stopEventHandle);
+    free(details->watchedPath);
+    env->DeleteGlobalRef(details->watcherCallback);
     free(details);
 }
 
