39dda8cf3b69e1afe3dcf54f4a44ff8f18cbc5b0
Adam Murdoch <adam@gradle.com>
2019-12-12 17:51:55 +1100

    Remove duplicate comment.

--- src/main/cpp/apple_fsnotifier.cpp
+++ src/main/cpp/apple_fsnotifier.cpp
@@ -1,5 +1,5 @@
 /*
- Initial version copied from Initial version copied from https://github.com/JetBrains/intellij-community/blob/master/native/fsNotifier/mac/fsnotifier.c
+ * Initial version copied from https://github.com/JetBrains/intellij-community/blob/master/native/fsNotifier/mac/fsnotifier.c
  */
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
