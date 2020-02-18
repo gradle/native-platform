f5b9d925089aa633d888c26a4950e20451a91c9e
Lóránt Pintér <lorant@gradle.com>
2020-02-18 12:34:59 +0100

    Move macOS headers to separate header file

---
+++ src/main/headers/apple_fsnotifier.h
@@ -0,0 +1,71 @@
+#if defined(__APPLE__)
+
+#include "generic.h"
+#include "net_rubygrapefruit_platform_internal_jni_OsxFileEventFunctions.h"
+#include <CoreServices/CoreServices.h>
+#include <thread>
+
+using namespace std;
+
+struct FileWatcherException : public exception {
+public:
+    FileWatcherException(const char* message) {
+        this->message = message;
+    }
+
+    const char* what() const throw() {
+        return message;
+    }
+
+private:
+    const char* message;
+};
+
+class Server;
+
+static void handleEventsCallback(
+    ConstFSEventStreamRef streamRef,
+    void* clientCallBackInfo,
+    size_t numEvents,
+    void* eventPaths,
+    const FSEventStreamEventFlags eventFlags[],
+    const FSEventStreamEventId eventIds[]);
+
+class Server {
+public:
+    Server(JNIEnv* env, jobject watcherCallback, CFArrayRef rootsToWatch, long latencyInMillis);
+    ~Server();
+
+private:
+    void run();
+
+    void handleEvents(
+        size_t numEvents,
+        char** eventPaths,
+        const FSEventStreamEventFlags eventFlags[],
+        const FSEventStreamEventId eventIds[]);
+    friend void handleEventsCallback(
+        ConstFSEventStreamRef streamRef,
+        void* clientCallBackInfo,
+        size_t numEvents,
+        void* eventPaths,
+        const FSEventStreamEventFlags eventFlags[],
+        const FSEventStreamEventId eventIds[]);
+
+    void handleEvent(JNIEnv* env, char* path, FSEventStreamEventFlags flags);
+
+    // TODO: Move this to somewhere else
+    JNIEnv* getThreadEnv();
+
+    JavaVM* jvm;
+    jobject watcherCallback;
+    jmethodID watcherCallbackMethod;
+
+    FSEventStreamRef watcherStream;
+    thread watcherThread;
+    mutex watcherThreadMutex;
+    condition_variable watcherThreadStarted;
+    CFRunLoopRef threadLoop;
+};
+
+#endif
