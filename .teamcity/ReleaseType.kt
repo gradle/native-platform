1b85dec30e65cd5687af141b519c1d2fdac55671
Stefan Wolf <wolf@gradle.com>
2020-02-19 22:29:12 +0100

    Add credentials for releasing

--- .teamcity/ReleaseType.kt
+++ .teamcity/ReleaseType.kt
@@ -1,6 +1,6 @@
 enum class ReleaseType(val gradleProperty: String, val username: String, val password: String, val userProvidedVersion: Boolean = false) {
     Snapshot("snapshot", "bot-build-tool", "credentialsJSON:d94612fb-3291-41f5-b043-e2b3994aeeb4"),
     Alpha("alpha", "bot-build-tool", "credentialsJSON:d94612fb-3291-41f5-b043-e2b3994aeeb4", true),
-    Milestone("milestone", "bot-build-tool", "credentialsJSON:d94612fb-3291-41f5-b043-e2b3994aeeb4"),
-    Release("release", "bot-build-tool", "credentialsJSON:d94612fb-3291-41f5-b043-e2b3994aeeb4")
+    Milestone("milestone", "adammurdoch", "credentialsJSON:762b676d-f41d-434b-979f-6d3d76694033"),
+    Release("release", "adammurdoch", "credentialsJSON:762b676d-f41d-434b-979f-6d3d76694033")
 }
