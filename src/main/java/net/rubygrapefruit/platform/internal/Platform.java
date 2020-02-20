6f3c73ab5226f9f6d9378957a2ac45271cad5351
Stefan Wolf <wolf@gradle.com>
2020-02-20 14:09:50 +0100

    Create new library for file events

--- src/main/java/net/rubygrapefruit/platform/internal/Platform.java
+++ src/main/java/net/rubygrapefruit/platform/internal/Platform.java
@@ -117,6 +117,11 @@ public abstract class Platform {
                 toString()));
     }
 
+    public String getFileEventsLibraryName() {
+        throw new NativeIntegrationUnavailableException(String.format("Native file events integration is not available for %s.",
+                toString()));
+    }
+
     public List<String> getLibraryVariants() {
         return Arrays.asList(getId());
     }
@@ -140,6 +145,11 @@ public abstract class Platform {
         @Override
         public String getLibraryName() {
             return "native-platform.dll";
+        }
+
+        @Override
+        public String getFileEventsLibraryName() {
+            return "native-platform-file-events.dll";
         }
 
         @Override
@@ -260,6 +270,11 @@ public abstract class Platform {
         String getCursesLibraryName() {
             return "libnative-platform-curses.so";
         }
+
+        @Override
+        public String getFileEventsLibraryName() {
+            return "libnative-platform-file-events.so";
+        }
     }
 
     private abstract static class Linux extends Unix {
@@ -338,6 +353,11 @@ public abstract class Platform {
         }
 
         @Override
+        public String getFileEventsLibraryName() {
+            return "libnative-platform-file-events.dylib";
+        }
+
+        @Override
         String getCursesLibraryName() {
             return "libnative-platform-curses.dylib";
         }
@@ -351,6 +371,14 @@ public abstract class Platform {
                 return type.cast(new DefaultMemory());
             }
             if (type.equals(OsxFileEventFunctions.class)) {
+                nativeLibraryLoader.load(getFileEventsLibraryName(), getLibraryVariants());
+                // TODO: Check version
+//                String nativeVersion = TerminfoFunctions.getVersion();
+//                if (!nativeVersion.equals(NativeVersion.VERSION)) {
+//                    throw new NativeException(String.format(
+//                        "Unexpected native library version loaded. Expected %s, was %s.", nativeVersion,
+//                        NativeVersion.VERSION));
+//                }
                 return type.cast(new OsxFileEventFunctions());
             }
             return super.get(type, nativeLibraryLoader);
