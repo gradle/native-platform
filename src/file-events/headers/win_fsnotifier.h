562a4dfb45e158a25bad2c43ca4f15be90c8dbce
Lóránt Pintér <lorant@gradle.com>
2020-03-12 16:27:59 +0100

    Win: More robust shutdown sequence

--- src/file-events/headers/win_fsnotifier.h
+++ src/file-events/headers/win_fsnotifier.h
@@ -22,15 +22,44 @@ using namespace std;
 
 #define EVENT_MASK (FILE_NOTIFY_CHANGE_FILE_NAME | FILE_NOTIFY_CHANGE_DIR_NAME | FILE_NOTIFY_CHANGE_ATTRIBUTES | FILE_NOTIFY_CHANGE_SIZE | FILE_NOTIFY_CHANGE_LAST_WRITE)
 
+// TODO Make this parametrizable perhaps
+#define SERVER_CLOSE_TIMEOUT_IN_MS 1000
+
 class Server;
 class WatchPoint;
+enum WatchPointStatus {
+    /**
+     * The watch point has been constructed, but not yet listening.
+     */
+    CONSTRUCTED,
+
+    /**
+     * The watch point is listening, expect events to arrive.
+     */
+    LISTENING,
+
+    /**
+     * The watch point has stopped listening and is handling events.
+     */
+    HANDLING,
+
+    /**
+     * The watch point has been cancelled, expect ERROR_OPERATION_ABORTED event.
+     */
+    CANCELLED,
+
+    /**
+     * The watch point has been cancelled, the ERROR_OPERATION_ABORTED event arrived; or starting the listener caused an error.
+     */
+    FINISHED
+};
 
 class WatchPoint {
 public:
     WatchPoint(Server* server, const u16string& path);
     ~WatchPoint();
-
     void listen();
+    void cancel();
 
 private:
     Server* server;
@@ -39,6 +68,7 @@ private:
     HANDLE directoryHandle;
     OVERLAPPED overlapped;
     vector<BYTE> buffer;
+    WatchPointStatus status;
 
     void handleEventsInBuffer(DWORD errorCode, DWORD bytesTransferred);
     friend static void CALLBACK handleEventCallback(DWORD errorCode, DWORD bytesTransferred, LPOVERLAPPED overlapped);
@@ -54,7 +84,6 @@ public:
     void terminate() override;
 
     void handleEvents(WatchPoint* watchPoint, DWORD errorCode, const vector<BYTE>& buffer, DWORD bytesTransferred);
-    void reportFinished(const u16string path);
 
 protected:
     void runLoop(JNIEnv* env, function<void(exception_ptr)> notifyStarted) override;
@@ -62,6 +91,8 @@ protected:
 
 private:
     void handleEvent(JNIEnv* env, const u16string& path, FILE_NOTIFY_INFORMATION* info);
+
+    unsigned int countOpenWatchPoints();
 
     unordered_map<u16string, WatchPoint> watchPoints;
     bool terminated = false;
