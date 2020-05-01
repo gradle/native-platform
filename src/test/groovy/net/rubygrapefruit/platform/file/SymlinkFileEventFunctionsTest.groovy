f5f94880178063fb331116ac0fd313717d9f9fb8
Lóránt Pintér <lorant.pinter@gmail.com>
2020-05-01 16:03:50 +0200

    Add tests for detecting symlink changes

--- src/test/groovy/net/rubygrapefruit/platform/file/SymlinkFileEventFunctionsTest.groovy
+++ src/test/groovy/net/rubygrapefruit/platform/file/SymlinkFileEventFunctionsTest.groovy
@@ -20,14 +20,82 @@ import net.rubygrapefruit.platform.NativeException
 import net.rubygrapefruit.platform.internal.Platform
 import spock.lang.Ignore
 import spock.lang.Requires
+import spock.lang.Unroll
 
 import static java.nio.file.Files.createSymbolicLink
 import static net.rubygrapefruit.platform.file.AbstractFileEventFunctionsTest.PlatformType.MAC_OS
 import static net.rubygrapefruit.platform.file.AbstractFileEventFunctionsTest.PlatformType.OTHERWISE
+import static net.rubygrapefruit.platform.file.FileWatchEvent.ChangeType.CREATED
 import static net.rubygrapefruit.platform.file.FileWatchEvent.ChangeType.MODIFIED
+import static net.rubygrapefruit.platform.file.FileWatchEvent.ChangeType.REMOVED
 
+@Unroll
 @Requires({ Platform.current().macOs || Platform.current().linux || Platform.current().windows })
 class SymlinkFileEventFunctionsTest extends AbstractFileEventFunctionsTest {
+
+    def "can detect symlink to #description created"() {
+        given:
+        def target = new File(rootDir, "target")
+        initialize(target)
+        def link = new File(rootDir, "link")
+        startWatcher(rootDir)
+
+        when:
+        createSymlink(link, target)
+
+        then:
+        expectEvents change(CREATED, link)
+
+        where:
+        description    | initialize
+        "regular file" | { File file -> assert file.createNewFile() }
+        "directory"    | { File file -> assert file.mkdirs() }
+        "missing file" | { File file -> }
+    }
+
+    def "can detect symlink to #description removed"() {
+        given:
+        def target = new File(rootDir, "target")
+        initialize(target)
+        def link = new File(rootDir, "link")
+        createSymlink(link, target)
+        startWatcher(rootDir)
+
+        when:
+        link.delete()
+
+        then:
+        expectEvents change(REMOVED, link)
+
+        where:
+        description    | initialize
+        "regular file" | { File file -> assert file.createNewFile() }
+        "directory"    | { File file -> assert file.mkdirs() }
+        "missing file" | { File file -> }
+    }
+
+    def "does not detect symlink target #description created"() {
+        given:
+        def contentRoot = new File(rootDir, "contentDir")
+        contentRoot.mkdirs()
+        def watchedRoot = new File(rootDir, "watchedDir")
+        watchedRoot.mkdirs()
+        def target = new File(contentRoot, "target")
+        def link = new File(watchedRoot, "link")
+        createSymlink(link, target)
+        startWatcher(watchedRoot)
+
+        when:
+        initialize(target)
+
+        then:
+        expectNoEvents()
+
+        where:
+        description    | initialize
+        "regular file" | { File file -> assert file.createNewFile() }
+        "directory"    | { File file -> assert file.mkdirs() }
+    }
 
     def "can detect changes in symlinked watched directory"() {
         given:
@@ -35,7 +103,7 @@ class SymlinkFileEventFunctionsTest extends AbstractFileEventFunctionsTest {
         createNewFile(canonicalFile)
         def watchedLink = new File(testDir, "linked")
         def watchedFile = new File(watchedLink, "modified.txt")
-        createSymbolicLink(watchedLink.toPath(), rootDir.toPath())
+        createSymlink(watchedLink, rootDir)
         startWatcher(watchedLink)
 
         when:
@@ -43,8 +111,8 @@ class SymlinkFileEventFunctionsTest extends AbstractFileEventFunctionsTest {
 
         then:
         expectEvents byPlatform(
-            (MAC_OS):     [change(MODIFIED, canonicalFile)],
-            (OTHERWISE) : [change(MODIFIED, watchedFile)]
+            (MAC_OS): [change(MODIFIED, canonicalFile)],
+            (OTHERWISE): [change(MODIFIED, watchedFile)]
         )
     }
 
@@ -57,7 +125,7 @@ class SymlinkFileEventFunctionsTest extends AbstractFileEventFunctionsTest {
         createNewFile(canonicalFile)
         def linkedDir = new File(rootDir, "linked")
         def linkedFile = new File(linkedDir, "modified.txt")
-        createSymbolicLink(linkedDir.toPath(), canonicalDir.toPath())
+        createSymlink(linkedDir, canonicalDir)
         startWatcher(canonicalDir, linkedDir)
 
         when:
@@ -65,7 +133,7 @@ class SymlinkFileEventFunctionsTest extends AbstractFileEventFunctionsTest {
 
         then:
         expectEvents byPlatform(
-            (MAC_OS):    [change(MODIFIED, canonicalFile), change(MODIFIED, canonicalFile)],
+            (MAC_OS): [change(MODIFIED, canonicalFile), change(MODIFIED, canonicalFile)],
             (OTHERWISE): [change(MODIFIED, linkedFile), change(MODIFIED, canonicalFile)]
         )
     }
@@ -79,7 +147,7 @@ class SymlinkFileEventFunctionsTest extends AbstractFileEventFunctionsTest {
         def canonicalDir = new File(rootDir, "watchedDir")
         canonicalDir.mkdirs()
         def linkedDir = new File(rootDir, "linked")
-        createSymbolicLink(linkedDir.toPath(), canonicalDir.toPath())
+        createSymlink(linkedDir, canonicalDir)
 
         when:
         startWatcher(canonicalDir, linkedDir)
@@ -94,6 +162,12 @@ class SymlinkFileEventFunctionsTest extends AbstractFileEventFunctionsTest {
         then:
         ex = thrown NativeException
         ex.message == "Already watching path: ${canonicalDir.absolutePath}"
+    }
+
+    private void createSymlink(File linked, File target) {
+        LOGGER.info("> Creating link to ${shorten(target)} in ${shorten(linked)}")
+        createSymbolicLink(linked.toPath(), target.toPath())
+        LOGGER.info("< Created link to ${shorten(target)} in ${shorten(linked)}")
     }
 
 }
