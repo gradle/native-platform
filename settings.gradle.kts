e4c5208c604b5f23b41198d3ed32b479b2de21f9
Stefan Wolf <wolf@gradle.com>
2020-07-02 18:49:23 +0200

    Use Gradle Enterprise convention plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     `gradle-enterprise`
+    id("com.gradle.enterprise.gradle-enterprise-conventions-plugin").version("0.2")
 }
 
 rootProject.name = "native-platform"
@@ -9,20 +10,3 @@ include("testApp")
 project(":testApp").projectDir = File(rootDir, "test-app")
 
 enableFeaturePreview("GROOVY_COMPILATION_AVOIDANCE")
-
-gradleEnterprise {
-    buildScan {
-        isCaptureTaskInputFiles = true
-        if (!System.getenv("CI").isNullOrEmpty()) {
-            tag("CI")
-            link("TeamCity Build", System.getenv("BUILD_URL"))
-            value("BuildId", System.getenv("BUILD_ID"))
-            val commitId = System.getenv("BUILD_VCS_NUMBER")
-            value("GitCommit", commitId)
-            link("Source", "https://github.com/gradle/native-platform/commit/$commitId")
-            isUploadInBackground = false
-        } else {
-            tag("local")
-        }
-    }
-}
