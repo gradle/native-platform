bec849126ea6ab25305bcca6ad09aad707f689e8
Stefan Wolf <wolf@gradle.com>
2020-08-07 16:42:26 +0200

    Move curses and freebsd platforms out of JNI-Plugin
    We only need those for native platform,
    not for file-system watching.

--- buildSrc/src/main/java/JniPlugin.java
+++ buildSrc/src/main/java/JniPlugin.java
@@ -42,7 +42,6 @@ import org.gradle.nativeplatform.platform.internal.DefaultNativePlatform;
 import org.gradle.nativeplatform.tasks.LinkSharedLibrary;
 import org.gradle.nativeplatform.toolchain.Clang;
 import org.gradle.nativeplatform.toolchain.Gcc;
-import org.gradle.nativeplatform.toolchain.GccPlatformToolChain;
 import org.gradle.nativeplatform.toolchain.NativeToolChainRegistry;
 import org.gradle.nativeplatform.toolchain.VisualCpp;
 import org.gradle.platform.base.BinarySpec;
@@ -235,16 +234,11 @@ public abstract class JniPlugin implements Plugin<Project> {
         void createPlatforms(PlatformContainer platformContainer) {
             addPlatform(platformContainer, "osx_amd64", "osx", "amd64");
             addPlatform(platformContainer, "linux_amd64", "linux", "amd64");
-            addPlatform(platformContainer, "linux_amd64_ncurses5", "linux", "amd64");
-            addPlatform(platformContainer, "linux_amd64_ncurses6", "linux", "amd64");
             addPlatform(platformContainer, "linux_aarch64", "linux", "aarch64");
-            addPlatform(platformContainer, "linux_aarch64_ncurses5", "linux", "aarch64");
-            addPlatform(platformContainer, "linux_aarch64_ncurses6", "linux", "aarch64");
             addPlatform(platformContainer, "windows_i386", "windows", "i386");
             addPlatform(platformContainer, "windows_amd64", "windows", "amd64");
             addPlatform(platformContainer, "windows_i386_min", "windows", "i386");
             addPlatform(platformContainer, "windows_amd64_min", "windows", "amd64");
-            addPlatform(platformContainer, "freebsd_amd64_libcpp", "freebsd", "amd64");
         }
 
         private void addPlatform(PlatformContainer platformContainer, String name, String os, String architecture) {
@@ -259,55 +253,9 @@ public abstract class JniPlugin implements Plugin<Project> {
                 // The core Gradle toolchain for gcc only targets x86 and x86_64 out of the box.
                 // https://github.com/gradle/gradle/blob/36614ee523e5906ddfa1fed9a5dc00a5addac1b0/subprojects/platform-native/src/main/java/org/gradle/nativeplatform/toolchain/internal/gcc/AbstractGccCompatibleToolChain.java
                 toolChain.target("linux_aarch64");
-                toolChain.target("linux_aarch64_ncurses5");
-                toolChain.target("linux_aarch64_ncurses6");
-                toolChain.eachPlatform(platformToolChain -> {
-                    // Use GCC to build for libstdc++ on FreeBSD
-                    NativePlatform platform = platformToolChain.getPlatform();
-                    if (platform.getOperatingSystem().isFreeBSD()) {
-                        if (platform.getName().contains("libstdcpp")) {
-                            enableToolChain(platformToolChain);
-                        } else {
-                            disableToolChain(platformToolChain);
-                        }
-                    }
-
-                });
             });
-            toolChainRegistry.create("clang", Clang.class, toolChain -> {
-                // The core Gradle toolchain for gcc only targets x86 and x86_64 out of the box.
-                // https://github.com/gradle/gradle/blob/36614ee523e5906ddfa1fed9a5dc00a5addac1b0/subprojects/platform-native/src/main/java/org/gradle/nativeplatform/toolchain/internal/gcc/AbstractGccCompatibleToolChain.java
-                toolChain.eachPlatform(platformToolChain -> {
-                    // Use GCC to build for libstdc++ on FreeBSD
-                    NativePlatform platform = platformToolChain.getPlatform();
-                    if (platform.getOperatingSystem().isFreeBSD()) {
-                        if (platform.getName().contains("libcpp")) {
-                            enableToolChain(platformToolChain);
-                        } else {
-                            // Use a dummy so that GCC is not selected
-                            disableToolChain(platformToolChain);
-                        }
-                    }
-                });
-            });
+            toolChainRegistry.create("clang", Clang.class);
             toolChainRegistry.create("visualCpp", VisualCpp.class);
-        }
-
-        private void disableToolChain(GccPlatformToolChain platformToolChain) {
-            // Use a dummy so that GCC is not selected
-            platformToolChain.getcCompiler().setExecutable("dummy");
-            platformToolChain.getCppCompiler().setExecutable("dummy");
-            platformToolChain.getObjcCompiler().setExecutable("dummy");
-            platformToolChain.getObjcppCompiler().setExecutable("dummy");
-            platformToolChain.getAssembler().setExecutable("dummy");
-            platformToolChain.getLinker().setExecutable("dummy");
-            platformToolChain.getStaticLibArchiver().setExecutable("dummy");
-        }
-
-        private void enableToolChain(GccPlatformToolChain platformToolChain) {
-            platformToolChain.getcCompiler().setExecutable("cc");
-            platformToolChain.getCppCompiler().setExecutable("c++");
-            platformToolChain.getLinker().setExecutable("c++");
         }
 
         @Mutate
