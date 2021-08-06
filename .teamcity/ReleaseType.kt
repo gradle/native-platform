8270bf1adc3d8e16dbac58f6437ad6e42ea61027
Stefan Wolf <wolf@gradle.com>
2021-08-06 17:26:02 +0200

    Publish to internal Gradle repository

--- .teamcity/ReleaseType.kt
+++ .teamcity/ReleaseType.kt
@@ -1,6 +1,6 @@
 enum class ReleaseType(val gradleProperty: String, val username: String, val password: String, val userProvidedVersion: Boolean = false) {
-    Snapshot("snapshot", "bot-build-tool", "credentialsJSON:d94612fb-3291-41f5-b043-e2b3994aeeb4"),
-    Alpha("alpha", "bot-build-tool", "credentialsJSON:d94612fb-3291-41f5-b043-e2b3994aeeb4", true),
-    Milestone("milestone", "bot-build-tool", "credentialsJSON:d94612fb-3291-41f5-b043-e2b3994aeeb4"),
-    Release("release", "bot-build-tool", "credentialsJSON:d94612fb-3291-41f5-b043-e2b3994aeeb4")
+    Snapshot("snapshot", "bot-build-tool", "credentialsJSON:fc942743-b732-4204-a131-26bb066c2073"),
+    Alpha("alpha", "bot-build-tool", "credentialsJSON:fc942743-b732-4204-a131-26bb066c2073", true),
+    Milestone("milestone", "bot-build-tool", "credentialsJSON:fc942743-b732-4204-a131-26bb066c2073"),
+    Release("release", "bot-build-tool", "credentialsJSON:fc942743-b732-4204-a131-26bb066c2073")
 }
