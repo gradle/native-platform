9984b338e0062305acc1cced78ceafde5cb1deea
Christoph Obexer <cobexer@gradle.com>
2024-07-23 03:27:42 +0200

    CI: make sure CI always picks CentOS Steam 9 (#336)
    Co-authored-by: Bo Zhang <bo@gradle.com>

--- .teamcity/Os.kt
+++ .teamcity/Os.kt
@@ -36,6 +36,7 @@ interface Os {
     object CentOs : Linux(Ncurses.Ncurses6) {
         override fun Requirements.additionalRequirements() {
             contains(osDistributionNameParameter, "centos")
+            contains(osDistributionVersionParameter, "9")
         }
     }
 
