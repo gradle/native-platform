35eb0a4d80d65878d36bcb1d095e8675a429e9da
Christoph Obexer <cobexer@gradle.com>
2024-10-28 15:59:29 +0100

    Update Develocity Plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.gradle.develocity").version("3.17.5")
+    id("com.gradle.develocity").version("3.18.1")
     id("io.github.gradle.gradle-enterprise-conventions-plugin").version("0.10.1")
 }
 
