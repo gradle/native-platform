ccc7032db80e2f577a6e80cb57919f9b71fa8aaf
Christoph Obexer <cobexer@gradle.com>
2024-10-28 15:59:41 +0100

    Update conventions Plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.gradle.develocity").version("3.18.1")
-    id("io.github.gradle.gradle-enterprise-conventions-plugin").version("0.10.1")
+    id("io.github.gradle.gradle-enterprise-conventions-plugin").version("0.10.2")
 }
 
 rootProject.name = "native-platform"
